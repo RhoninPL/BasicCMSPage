d9618a0623e6e87afde0a871fd9dd3b70b87f325
Michał Świtalik <michal.switalik@gmail.com>
2016-06-08 08:36:21 +0200

    Create solution

---
+++ BasicCMSPage/ViewModels/ContentEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicCMSPage.ViewModels
+{
+    public class ContentEditViewModel
+    {
+        public string Name;
+        public string Content;
+        public bool Visible;
+        public DateTime LastModify;
+        public DateTime CreationDateTime;
+    }
+}
---
+++ BasicCMSPage/ViewModels/ContentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicCMSPage.ViewModels
+{
+    public class ContentViewModel
+    {
+        public string Name;
+        public string Content;
+    }
+}
